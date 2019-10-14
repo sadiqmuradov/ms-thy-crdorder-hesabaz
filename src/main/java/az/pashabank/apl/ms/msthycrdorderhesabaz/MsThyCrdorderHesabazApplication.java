8c7b0b075c70fedcf4f74533a8d65889e19addc1
Mahmud Aliyev <Mahmud.Aliyev@pashabank.az>
2019-10-14 12:35:14 +0400

    initial commit

--- src/main/java/az/pashabank/apl/ms/msthycrdorderhesabaz/MsThyCrdorderHesabazApplication.java
+++ src/main/java/az/pashabank/apl/ms/msthycrdorderhesabaz/MsThyCrdorderHesabazApplication.java
@@ -2,8 +2,10 @@ package az.pashabank.apl.ms.msthycrdorderhesabaz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("az.pashabank.apl.ms.msthycrdorderhesabaz.proxy")
 public class MsThyCrdorderHesabazApplication {
 
     public static void main(String[] args) {
